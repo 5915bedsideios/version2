e860fa573a98148abe5b7df2da639bb816027ddd
Member 1 <osu5915bedside@gmail.com>
2020-11-12 20:09:36 -0500

    Small updates

---
+++ BedSide Chats/BedSide Chats/GuestController.swift
@@ -0,0 +1,19 @@
+//
+//  GuestController.swift
+//  BedSide Chats
+//
+//
+
+import SwiftUI
+
+struct GuestController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GuestController_Previews: PreviewProvider {
+    static var previews: some View {
+        GuestController()
+    }
+}
